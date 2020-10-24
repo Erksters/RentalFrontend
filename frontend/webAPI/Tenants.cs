3a6ef766335255b1964468d2026abe6f0172c03a
Carlos Cerna <cbcerna@ksu.edu>
2020-10-23 21:56:16 -0500

    test

---
+++ frontend/webAPI/Tenants.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webAPI{
+    public class Tenants{
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public int CreditScore { get; set; }
+    }
+}

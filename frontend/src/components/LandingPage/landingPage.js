12d5437ad9068cdf5e2da313557b88ef4b230fca
Erick PC <erksters@gmail.com>
2020-10-24 20:18:05 -0500

    Added a little template for filtering on landing page

--- frontend/src/components/LandingPage/landingPage.js
+++ frontend/src/components/LandingPage/landingPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Collapse } from "react-bootstrap";
 
 const LandingPage = () => {
     const sampleImage = "https://apollostore.blob.core.windows.net/multifamilybiz/News/Verve_Luxury_Apartments.jpg";
@@ -7,13 +7,29 @@ const LandingPage = () => {
     const [City, setCity] = useState("");
     const [MonthlyPrice, setMonthlyPrice] = useState(0)
     const [Available, setAvailable] = useState(true);
-    const [YearBuilt, setYearBuilt] = useState(undefined);
+    const [YearBuilt, setYearBuilt] = useState(1900);
     const [CentralAC, setCentralAC] = useState(false);
     const [NumberRooms, setNumberRooms] = useState(0);
     const [NumberBathrooms, setNumberBathrooms] = useState(0);
     const [Refurbished, setRefurbished] = useState(false);
     const [PetsAllowed, setPetsAllowed] = useState(false);
-    const [listOfApartments, setListOfApartments] = useState([1]);
+
+    const [searchByZipCode, setSeachByZipcode] = useState(true);
+    const [searchByCity, setsearchByCity] = useState(false);
+    const [searchByMonthlyPrice, setsearchByMonthlyPrice] = useState(true);
+    const [searchByAvailable, setSearchByAvailable] = useState(false);
+    const [searchByYearBuilt, setsearchBySearchByYearBuilt] = useState(false);
+    const [searchByCentralAC, setSearchByCentralAC] = useState(false);
+    const [searchByNumberBathrooms, setsearchByNumberBathrooms ] = useState(false);
+    const [searchByNumberRooms, setsearchByNumberRooms ] = useState(false);
+    const [searchByRefurbished, setSearchByRefurbished ] = useState(false);
+    const [searchByPetsAllowed, setsearchByPetsAllowed] = useState(false);
+
+    const [listOfApartments] = useState([1]);
+
+
+    const [open, setOpen] = useState(false);
+
 
     const handleReset = () => {
          setZipcode(66502);
@@ -28,11 +44,15 @@ const LandingPage = () => {
          setPetsAllowed(false);
     }
 
+    const handleSubmit = () => {
+
+    }
+
     const loadData = async () => {
         const filterData = new FormData();
         filterData.append("city", City);
         filterData.append("zipcode", ZipCode);
-        if(YearBuilt !== undefine){
+        if(YearBuilt !== undefined){
             filterData.append("YearBuilt", YearBuilt);
         }else{filterData.append("YearBuilt",undefined)}
         filterData.append("centralac", CentralAC);
@@ -49,11 +69,7 @@ const LandingPage = () => {
             return response.json();
           })
           .then((data) => {
-            if (data.length > 0) {
-              setThisShirt([...data]);
-            } else {
-              setThisShirt(404);
-            }
+            
           })
           .catch((error) => {});
       };
@@ -65,7 +81,62 @@ const LandingPage = () => {
     return(
         <div>
             <div>
-                <Button onClick ={handleReset}>Reset Filters</Button>
+            <label className="pr-2 labels">City</label>
+            <input type="text" onChange={(e) => setCity(e.target.value)} />
+            <br/>
+            <label className="pr-2 labels">ZipCode</label>
+            <input type="number" onChange={(e) => setZipcode(e.target.value)} />
+            <br/>
+
+            <label className="pr-2 labels">Available</label>
+            <input type="checkbox" onChange={(e) => setAvailable(e.target.value)} />
+            <br/>
+
+            <label className="pr-2 labels">YearBuilt</label>
+            <input type="number" onChange={(e) => setYearBuilt(e.target.value)} />
+            <br/>
+
+            {/* TODO Date Available */}
+
+            <label className="pr-2 labels">Monthly Price</label>
+            <input type="number" onChange={(e) => setMonthlyPrice(e.target.value)} />
+            <br/>
+
+            <label className="pr-2 labels">Central AC</label>
+            <input type="checkbox" onChange={(e) => setCentralAC(e.target.value)} />
+            <br/>
+
+            <label className="pr-2 labels">Number Of Rooms</label>
+            <input type="number" onChange={(e) => setNumberRooms(e.target.value)} />
+            <br/>
+
+            <label className="pr-2 labels">Number of Bathrooms</label>
+            <input type="number" onChange={(e) => setNumberBathrooms(e.target.value)} />
+            <br/>
+{/* 
+            <label className="pr-2 labels">Refurbished</label>
+            <input type="checkbox" value={searchByRefurbished} onChange={(e) => setSearchByRefurbished(e.target.value)} />
+            {searchByRefurbished && 
+                <input type="checkbox" onChange={(e) => setRefurbished(e.target.value)} />
+            }
+            <br/> */}
+            
+            
+        
+
+            <Button onClick={() => setsearchByPetsAllowed(!searchByPetsAllowed)}>
+                Pets
+            </Button>
+            <Collapse in={searchByPetsAllowed}>
+                <div >
+                <label >{PetsAllowed ? "Pets allowed": "No Pets allowed"}</label>
+                        <input type="checkbox" onClick={(e) => setPetsAllowed(!PetsAllowed)} />
+                </div>
+            </Collapse>
+            <br/>
+            
+            <Button onClick={handleSubmit}>Search</Button>
+            <Button onClick ={handleReset}>Reset Filters</Button>
             </div>
         </div>
     );

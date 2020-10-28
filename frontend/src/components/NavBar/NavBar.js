5a96b57b155dcfdbc783b8a0abe0b17452ca58b0
Erksters <51130557+Erksters@users.noreply.github.com>
2020-10-27 22:09:40 -0500

    Added Navigation bar to app.js (#7)

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Navbar, NavDropdown, Nav } from "react-bootstrap";
+
+const NavigationBar = (props) => {
+
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">K-State Rentals</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/" >Home</Nav.Link>
+          <NavDropdown title="Tenants" id="basic-nav-dropdown">          
+            <Nav.Link href="/create/tenant">Become a Tenant</Nav.Link>
+            <Nav.Link href="/update/tenant">Update Tenant info</Nav.Link>
+            <Nav.Link href="/">Pay your bill</Nav.Link>
+          </NavDropdown>
+          
+          <NavDropdown title="Property Owners" id="basic-nav-dropdown">          
+            <Nav.Link href="/create/propetyowner">Become a property Owner</Nav.Link>
+            <Nav.Link href="/update/propetyowner">Update Property Owner info</Nav.Link>
+            <Nav.Link href="/">Sign In</Nav.Link>
+          </NavDropdown>
+
+          <NavDropdown title="Admin" id="basic-nav-dropdown">          
+            <Nav.Link href="/report/Queries">Report Queries</Nav.Link>
+            <Nav.Link href="/create/Contract">Create Contract</Nav.Link>
+            <Nav.Link href="/create/ContractAgreements">Create Contract Agreements</Nav.Link>
+            <Nav.Link href="/credit/score">Add Credit Score</Nav.Link>
+          </NavDropdown>
+          <Nav.Link href="/">Sign In</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
